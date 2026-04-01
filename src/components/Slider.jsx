685534e8203f1e8cb0535f59986f7e769a819ae1
Jake-College <mccallen01@allegheny.edu>
2026-04-01 15:16:03 -0400

    the buttons refuse

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,13 +1,45 @@
-import React, {useState, useTransition, useEffect, Suspense} from 'react';
+import React, {useState, useEffect} from 'react';
 
-const Slider = ({children}) => {
-    const [currentImage, setCurrentImage] = useState(0);
+const Slider = ({slides}) => {
+    const [currentIndex, setCurrentIndex] = useState(0);
 
-    return(
-        <Suspense>
-            {children}
-        </Suspense>
-    )
+    const autoScroll = () => {
+        if (currentIndex === slides.length -1) {
+            return setCurrentIndex(0);
+        }
+        return setCurrentIndex(currentIndex + 1);
+    }
+
+    useEffect(() => {
+        const interval = setInterval(() => {autoScroll()}, 3000);
+        return () => clearInterval(interval);
+    })
+
+    return (
+        <div className = "image-slider">
+            <div className = "Buttons">
+                <button class = "Left-Button">
+                    <i class="fa-solid fa-circle-arrow-left"></i>
+                </button>
+                <button class = "Right-Button">
+                    <i class="fa-solid fa-circle-arrow-right"></i>
+                </button>
+            </div>
+            <ul>
+                {slides.map((slide, index) => (
+                    <li key = {index} className = {index === currentIndex ? "active" : ""}>
+                        <img 
+                            src = {slide.src} 
+                            alt = {slide.alt} 
+                            style = {{
+                                transform: `translateX(calc(-${currentIndex * 100}% - ${currentIndex * 3}rem))`
+                            }}
+                        />
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
 }
 
 export default Slider;
