f2cad0b27776ef73590136a8b0f0c0dc25ff7378
Jake-College <mccallen01@allegheny.edu>
2026-04-13 11:54:08 -0400

    ?

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -2,34 +2,34 @@ import React, {useState, useEffect} from 'react';
 
 const Slider = ({slides}) => {
     const [currentIndex, setCurrentIndex] = useState(0);
-    ```
-    const autoScroll = () => {
-        if (currentIndex === slides.length -1) {
-            return setCurrentIndex(0);
-        }
-        return setCurrentIndex(currentIndex + 1);
-    }
-    ```
+    
+    // const autoScroll = () => {
+    //     if (currentIndex === slides.length -1) {
+    //         return setCurrentIndex(0);
+    //     }
+    //     return setCurrentIndex(currentIndex + 1);
+    // }
+    
     const buttonPush = () => {
-        const clicked = (evt) => {
+        const clickedR = (evt) => {
             if (currentIndex === slides.length -1) {
                 return setCurrentIndex(0);
             }
-            return setCurrentIndex(currentIndex + 1);
+            return setCurrentIndex(currentIndex + 1)
         }
     }
-    ```
-    useEffect(() => {
-        const interval = setInterval(() => {autoScroll()}, 3000);
-        return () => clearInterval(interval);
-    })
-    ```
+    
+    // useEffect(() => {
+    //     const interval = setInterval(() => {autoScroll()}, 3000);
+    //     return () => clearInterval(interval);
+    // })
+    
     return (
         <div className = "image-slider">
                 <button class = "Left-Button">
                     <i class="fa-solid fa-circle-arrow-left"></i>
                 </button>
-                <button class = "Right-Button">
+                <button class = "Right-Button" onClick={clickedR}>
                     <i class="fa-solid fa-circle-arrow-right"></i>
                 </button>
             <ul>
